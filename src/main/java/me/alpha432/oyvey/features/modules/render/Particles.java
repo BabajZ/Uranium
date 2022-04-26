1d83d5590e854d55109b4ca26a07076ae7100fd0
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-04-26 18:29:59 +0300

    3nd commit lol

--- src/main/java/me/alpha432/oyvey/features/modules/render/Particles.java
+++ src/main/java/me/alpha432/oyvey/features/modules/render/Particles.java
@@ -20,6 +20,13 @@ public class Particles  extends Module {
     public Setting<Boolean> pull3 = this.register(new Setting<Boolean>("FireWork", true));
     public Setting<Boolean> pull4 = this.register(new Setting<Boolean>("Portal", true));
     public Setting<Boolean> pull5 = this.register(new Setting<Boolean>("Spit", true));
+    public Setting<Boolean> b1 = this.register(new Setting<Boolean>("Slime", true));
+    public Setting<Boolean> b2 = this.register(new Setting<Boolean>("DragonBreath", true));
+    public Setting<Boolean> b3 = this.register(new Setting<Boolean>("EndRod", true));
+    public Setting<Boolean> b4 = this.register(new Setting<Boolean>("Totem", true));
+    public Setting<Boolean> b5 = this.register(new Setting<Boolean>("Bubble", true));
+    public Setting<Boolean> b6 = this.register(new Setting<Boolean>("SnowBall", true));
+
 
     public Particles() {
         super("Particles", "Spoofs The Haste Effect", Category.RENDER, true, false, false);
@@ -27,6 +34,24 @@ public class Particles  extends Module {
 
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent playerTickEvent) {
+        if (b6.getValue()) {
+            Particles.mc.world.spawnParticle(EnumParticleTypes.SNOWBALL, Particles.mc.player.posX, Particles.mc.player.posY + 0.2, Particles.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
+        }
+        if (b5.getValue()) {
+            Particles.mc.world.spawnParticle(EnumParticleTypes.WATER_BUBBLE, Particles.mc.player.posX, Particles.mc.player.posY + 0.2, Particles.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
+        }
+        if (b4.getValue()) {
+            Particles.mc.world.spawnParticle(EnumParticleTypes.TOTEM, Particles.mc.player.posX, Particles.mc.player.posY + 0.2, Particles.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
+        }
+        if (b3.getValue()) {
+            Particles.mc.world.spawnParticle(EnumParticleTypes.END_ROD, Particles.mc.player.posX, Particles.mc.player.posY + 0.2, Particles.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
+        }
+        if (b2.getValue()) {
+            Particles.mc.world.spawnParticle(EnumParticleTypes.DRAGON_BREATH, Particles.mc.player.posX, Particles.mc.player.posY + 0.2, Particles.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
+        }
+        if (b1.getValue()) {
+            Particles.mc.world.spawnParticle(EnumParticleTypes.SLIME, Particles.mc.player.posX, Particles.mc.player.posY + 0.2, Particles.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
+        }
         if (pull8.getValue()) {
             Particles.mc.world.spawnParticle(EnumParticleTypes.HEART, Particles.mc.player.posX, Particles.mc.player.posY + 0.2, Particles.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
